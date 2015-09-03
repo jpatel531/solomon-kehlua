bf73c0e29c7046440ebded95d8caffec3e38b722
Jamie Patel <jamie@notespublication.com>
2015-09-03 22:27:33 +0100

    Can specify version

--- kehlua.go
+++ kehlua.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mitchellh/goamz/s3"
 	"io/ioutil"
 	"path/filepath"
+	"regexp"
 	s "strings"
 )
 
@@ -18,8 +19,10 @@ type file struct {
 
 const permissions = s3.BucketOwnerFull
 
-func prepareFilesInDir(dirname string, files *[]file) {
-	dir, err := ioutil.ReadDir(dirname)
+var versionRegex = regexp.MustCompile(`\d+-\d+-\d+`)
+
+func prepareFilesInDir(localDir, s3Dir string, files *[]file) {
+	dir, err := ioutil.ReadDir(localDir)
 	if err != nil {
 		panic(err)
 	}
@@ -31,17 +34,18 @@ func prepareFilesInDir(dirname string, files *[]file) {
 			continue
 		}
 
-		path := fmt.Sprintf("%s/%s", dirname, name)
+		localPath := fmt.Sprintf("%s/%s", localDir, name)
+		s3Path := fmt.Sprintf("%s/%s", s3Dir, name)
 		if _file.IsDir() {
-			prepareFilesInDir(path, files)
+			prepareFilesInDir(localPath, s3Path, files)
 		} else {
-			data, fileErr := ioutil.ReadFile(path)
+			data, fileErr := ioutil.ReadFile(localPath)
 			if fileErr != nil {
 				panic(fileErr)
 			}
-			contentType := CONTENT_TYPE_LOOKUP[filepath.Ext(path)]
-			path = s.Replace(path, "./", "", 1)
-			item := file{path, data, contentType}
+			contentType := CONTENT_TYPE_LOOKUP[filepath.Ext(localPath)]
+			s3Path = s.Replace(s3Path, "./", "", 1)
+			item := file{s3Path, data, contentType}
 			*files = append(*files, item)
 		}
 	}
@@ -56,19 +60,26 @@ func upload(_file file, uploads chan<- bool, client *s3.S3, bucket *s3.Bucket) {
 func main() {
 
 	dirnamePtr := flag.String("folder", "", "the older where the assets reside")
+	versionPtr := flag.String("version", "", "the version of the styleguide")
 
 	flag.Parse()
 
 	dirname := *dirnamePtr
+	version := *versionPtr
 
 	if dirname == "" {
 		fmt.Println("`folder` must be specified")
 		return
 	}
 
+	if !versionRegex.MatchString(version) {
+		fmt.Println("`version` must be specified in the format major-minor-patch")
+		return
+	}
+
 	var files []file
 
-	prepareFilesInDir(dirname, &files)
+	prepareFilesInDir(dirname, version, &files)
 
 	requestNumber := len(files)
 
